36c1974418f688fddb0479ed5f39ad266541458c
aks- <suthar.ashok@outlook.com>
2014-09-02 23:25:05 +0530

    fix if statement

--- read_file_async.js
+++ read_file_async.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 console.log('Started.');
 
 var file = fs.readFile('./files/text_file.txt', function(err, data){
-    if(err != null) console.error(err);
+    if(err) console.error(err);
     console.log("Content: " + data);
 });
 
